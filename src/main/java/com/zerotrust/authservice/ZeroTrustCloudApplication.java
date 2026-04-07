edee43a28748d6160b6dc11bc3cdd35d4d15851c
SamyBaouche <samy.baouche@gmail.com>
2026-04-07 00:29:02 -0400

    Initial setup auth-service with PostgreSQL and Docker

--- src/main/java/com/zerotrust/authservice/ZeroTrustCloudApplication.java
+++ src/main/java/com/zerotrust/authservice/ZeroTrustCloudApplication.java
@@ -3,11 +3,12 @@ package com.zerotrust.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This annotation tells Spring Boot to start the application
 @SpringBootApplication
 public class ZeroTrustCloudApplication {
 
     public static void main(String[] args) {
+        // This line launches the Spring Boot application
         SpringApplication.run(ZeroTrustCloudApplication.class, args);
     }
-
 }
