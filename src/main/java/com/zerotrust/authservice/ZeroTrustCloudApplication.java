f9b066d890d14683fc0e37b5d0010b9f44079e1d
SamyBaouche <samy.baouche@gmail.com>
2026-04-07 00:17:10 -0400

    day1

---
+++ src/main/java/com/zerotrust/authservice/ZeroTrustCloudApplication.java
@@ -0,0 +1,13 @@
+package com.zerotrust.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroTrustCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroTrustCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerotrust/authservice/ZeroTrustCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerotrust.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroTrustCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
